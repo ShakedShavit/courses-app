32c55c1ee5c3fb76ec349201e9ca8c164d7ad83f
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-02-21 22:11:26 +0200

    main folder commit

--- server/app.js
+++ server/app.js
@@ -22,14 +22,14 @@ app.use(courseRouter);
 
 const path = require("path")
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
 
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
 
