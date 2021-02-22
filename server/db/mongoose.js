7f6fb0e257e90c66ad83450aa0afde1e301a42b3
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-02-22 15:22:44 +0200

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //connecting to the database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ShakedShav:47674674684902@cluster0.cx2o0.mongodb.net/courses-app?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://ShakedShav:47674674684902@cluster0.cx2o0.mongodb.net/courses-app?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
