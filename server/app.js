b716a3f89908a10bb6fdfbfd3940d1b35974c919
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-02-22 22:40:54 +0200

    A

--- server/app.js
+++ server/app.js
@@ -22,8 +22,8 @@ const path = require("path")
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../client", "build")));
-    app.use('*', express.static(path.join(__dirname, "../client", "build")))
+    app.use(express.static("client/build"));
+    app.use('*', express.static(path.resolve(__dirname, "../client", "build", "index.html")))
 }
 
 
