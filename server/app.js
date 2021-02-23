22abe3081c2361c84f05ace45267f4bcc3e31a3e
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-02-23 19:15:20 +0200

    A

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const cors = require('cors');
-// require('./db/mongoose');
+const cors = require('cors');
+require('./db/mongoose');
 const teacherRouter = require('./routes/teacher');
 const studentRouter = require('./routes/student');
 const courseRouter = require('./routes/course');
@@ -10,11 +10,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-// app.use(teacherRouter);
-// app.use(studentRouter);
-// app.use(courseRouter);
+app.use(teacherRouter);
+app.use(studentRouter);
+app.use(courseRouter);
 
 
 
