4cc4a5a6df3bf8b969ece8630763f2c62fec611c
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-02-23 17:23:23 +0200

    A

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 // require('./db/mongoose');
 const teacherRouter = require('./routes/teacher');
 const studentRouter = require('./routes/student');
@@ -10,11 +10,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-app.use(teacherRouter);
-app.use(studentRouter);
-app.use(courseRouter);
+// app.use(teacherRouter);
+// app.use(studentRouter);
+// app.use(courseRouter);
 
 
 
@@ -22,8 +22,11 @@ const path = require("path")
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
-    app.use('*', express.static(path.resolve(__dirname, "../client", "build", "index.html")))
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    });
 }
 
 
